8fb051e26a3f23f9ab34560dea7f8371eb201ff8
Hanna Pylieva <pylyeva@gmail.com>
2023-03-24 16:25:53 +0100

    Update UI.

--- app.py
+++ app.py
@@ -2,99 +2,70 @@
 
 """An example of showing geographic data."""
 
+import requests
+from io import BytesIO
+
 import streamlit as st
 import pandas as pd
 import numpy as np
-import altair as alt
-import pydeck as pdk
+import plotly.express as px
+from PIL import Image
+
+from src.data_filtering import filter_dataframe
 
 DATE_TIME = "date/time"
-DATA_URL = (
-    "http://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz"
+DATA_PATH = (
+    "../data/swansonhealth_2023-01-04_full_data.csv"
 )
 TOTAL_SAMPLES = 100000
+CLIENT_NAME = 'swansonhealth'
 
-st.title("Uber Pickups in New York City")
+st.set_page_config(page_title="Data exploration", page_icon="📊",  layout="wide")
+st.title(f"Persuasiveness exploration for {CLIENT_NAME}")
 st.markdown(
     """
-This is a demo of a Streamlit app that shows the Uber pickups
-geographical distribution in New York City. Use the slider
-to pick a specific hour and look at how the charts change.
-
-[See source code](https://github.com/streamlit/demo-uber-nyc-pickups/blob/master/app.py)
-"""
+    This is a visual tool to explore persuasivenss of different products and how is can be improved.
+    """
 )
 
 
 @st.cache(persist=True)
 def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    data.rename(lambda x: str(x).lower(), axis="columns", inplace=True)
-    data[DATE_TIME] = pd.to_datetime(data[DATE_TIME])
+    data = pd.read_csv(DATA_PATH, nrows=nrows)
+    # data.rename(lambda x: str(x).lower(), axis="columns", inplace=True)
     return data
 
 
 def run():
     data = load_data(TOTAL_SAMPLES)
+    st.dataframe(filter_dataframe(data))
+    url = st.text_input("Image URL", key="image_url")
 
-    hour = st.slider("Hour to look at", 0, 23)
+    if url != "":
+        try:
+            response = requests.get(url)
+            img = Image.open(BytesIO(response.content))
+            st.image(img)
+        except:
+            st.error("The URL does not seem to be valid.")
 
-    data = data[data[DATE_TIME].dt.hour == hour]
 
-    st.subheader("Geo data between %i:00 and %i:00" % (hour, (hour + 1) % 24))
-    midpoint = (np.average(data["lat"]), np.average(data["lon"]))
-
-    st.write(
-        pdk.Deck(
-            map_style="mapbox://styles/mapbox/light-v9",
-            initial_view_state={
-                "latitude": midpoint[0],
-                "longitude": midpoint[1],
-                "zoom": 11,
-                "pitch": 50,
-            },
-            layers=[
-                pdk.Layer(
-                    "HexagonLayer",
-                    data=data,
-                    get_position=["lon", "lat"],
-                    radius=100,
-                    elevation_scale=4,
-                    elevation_range=[0, 1000],
-                    pickable=True,
-                    extruded=True,
-                ),
-            ],
-        )
-    )
-
-    st.subheader(
-        "Breakdown by minute between %i:00 and %i:00" % (hour, (hour + 1) % 24)
-    )
-    filtered = data[
-        (data[DATE_TIME].dt.hour >= hour) & (data[DATE_TIME].dt.hour < (hour + 1))
-    ]
-    hist = np.histogram(filtered[DATE_TIME].dt.minute, bins=60, range=(0, 60))[0]
-    chart_data = pd.DataFrame({"minute": range(60), "pickups": hist})
-
-    st.altair_chart(
-        alt.Chart(chart_data)
-        .mark_area(
-            interpolate="step-after",
-        )
-        .encode(
-            x=alt.X("minute:Q", scale=alt.Scale(nice=False)),
-            y=alt.Y("pickups:Q"),
-            tooltip=["minute", "pickups"],
-        ),
-        use_container_width=True,
-    )
-
-    if st.checkbox("Show raw data", False):
+    if st.checkbox("Show histograms", False):
         st.subheader(
-            "Raw data by minute between %i:00 and %i:00" % (hour, (hour + 1) % 24)
+            "Histograms for features"
         )
-        st.write(data)
+        # create two columns for charts
+        fig_col1, fig_col2 = st.columns(2)
+
+        with fig_col1:
+            st.markdown("### Target distribution")
+            fig1 = px.histogram(data_frame=data, x="atc_price_range")
+            st.write(fig1)
+
+        with fig_col2:
+            st.markdown("### Second Chart")
+            fig2 = px.histogram(data_frame=data, x="add_to_cart_to_views")
+            st.write(fig2)
 
 
 if __name__ == "__main__":
