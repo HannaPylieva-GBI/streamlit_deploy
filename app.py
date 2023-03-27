9799f8f7e594a3f953e5dee7dd7aba1a58a4bea2
Hanna Pylieva <pylyeva@gmail.com>
2023-03-27 11:02:41 +0200

    Add selection of a feature for visualization and a scatterplot.

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ DATA_PATH = (
 )
 TOTAL_SAMPLES = 100000
 CLIENT_NAME = 'swansonhealth'
+TARGET_COL_NAME = 'add_to_cart_to_views'
 
 st.set_page_config(page_title="Data exploration", page_icon="📊",  layout="wide")
 st.title(f"Persuasiveness exploration for {CLIENT_NAME}")
@@ -49,23 +50,34 @@ def run():
         except:
             st.error("The URL does not seem to be valid.")
 
-
+    # with st.sidebar:
     if st.checkbox("Show histograms", False):
         st.subheader(
             "Histograms for features"
         )
         # create two columns for charts
-        fig_col1, fig_col2 = st.columns(2)
+        # fig_col1, fig_col2 = st.rows(2)
 
-        with fig_col1:
-            st.markdown("### Target distribution")
-            fig1 = px.histogram(data_frame=data, x="atc_price_range")
-            st.write(fig1)
+        # with fig_col1:
+        st.markdown("### Target distribution")
+        target_hist = px.histogram(data_frame=data, x=TARGET_COL_NAME)
+        st.write(target_hist)
 
-        with fig_col2:
-            st.markdown("### Second Chart")
-            fig2 = px.histogram(data_frame=data, x="add_to_cart_to_views")
-            st.write(fig2)
+        # with fig_col2:
+        st.markdown("### Features distribution")
+        columns = data.columns
+        make_col_choice = st.selectbox('Select the column to visualize distribution:', columns)
+        selected_column = data[make_col_choice]
+
+        col1, col2 = st.columns(2)
+
+        with col1:
+            feature_hist = px.histogram(data_frame=data, x=selected_column)
+            st.write(feature_hist)
+        with col2:
+            feature_scatter = px.scatter(data, TARGET_COL_NAME, selected_column)
+            st.write(feature_scatter)
+
 
 
 if __name__ == "__main__":
